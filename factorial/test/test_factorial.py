bd3f0d1256386551ff61246d6ab02007a48bfc1c
Rob Shield <rshield@theorchard.com>
2021-11-19 12:17:49 +0000

    removed tests

--- factorial/test/test_factorial.py
+++ factorial/test/test_factorial.py
@@ -1,20 +1,6 @@
 import pytest
 from factorial import factorial
 
-def test_fails_with_string_input():
-    try:
-        factorial.make_factorial('garbage')
-        assert 1 == 2
-    except IndexError as e:
-        assert True
-
-def test_fails_with_negative_number():
-    try:
-        factorial.make_factorial(-23)
-        assert 1 == 2
-    except TypeError as e:
-        assert True
-
 def test_factorial_zero():
     result = factorial.make_factorial(0)
     assert 1 == result
